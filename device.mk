333a7ef83191a48c931ac0aa1780a9149d55c2c2
prateekpunetha <hi@prateekpunetha.dev>
2020-10-07 11:19:09 +0200

    phoenix: cleanup: Remove unused aosip overlay
    Signed-off-by: prateekpunetha <hi@prateekpunetha.dev>

--- device.mk
+++ device.mk
@@ -100,8 +100,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosip
+    $(LOCAL_PATH)/overlay
 
 # Overlays - override vendor ones
 PRODUCT_PACKAGES += \

ba817e26bca64714e158cd4ca18281e8058a69aa
sarveshrulz <sarveshkardekar@gmail.com>
2020-11-15 13:48:27 +0000

    phoenix: adapt to aex

--- aosp_phoenix.mk
+++ aosp_phoenix.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosip_phoenix
+PRODUCT_NAME := aosp_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
